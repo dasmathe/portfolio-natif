250a1b43aa9f4e4b4e749af5a5661b8945f4b5f3
dasmathe <da.matheo@hotmail.com>
2025-02-25 14:50:35 +0100

    Test rendu php

---
+++ inc/footer.php
@@ -0,0 +1,40 @@
+<footer class="footer">
+        <div class="footer_bg">
+            <div class="footer_container container grid">
+                <div>
+                    <h1 class="footer_title">Mathéo</h1>
+                    <span class="footer_subtitle">Etudiant 3e année en BUT INFORMATIQUE</span>
+                </div>
+
+                <ul class="footer_links">
+                    <li>
+                        <a href="#about" class="footer_link">A propos de moi</a>
+                    </li>
+                    <li>
+                        <a href="#skills" class="footer_link">Mes compétences</a>
+                    </li>
+                    <li>
+                        <a href="#projects" class="footer_link">Mes projets</a>
+                    </li>
+                    <li>
+                        <a href="#contact" class="footer_link">Contactez-moi</a>
+                    </li>
+                </ul>
+
+                <div class="footer_socials">
+                    <a href="https://www.linkedin.com/in/mathéo-da-silva" target="_blank" class="footer_social">
+                        <i class="uil uil-linkedin-alt"></i>
+                    </a>
+                    <a href="https://www.youtube.com/channel/UCPTkJEYRPmzeZ_BLxemXW8Q" target="_blank"
+                        class="footer_social">
+                        <i class="uil uil-youtube"></i>
+                    </a>
+                    <a href="https://github.com/Senyotsu" target="_blank" class="footer_social">
+                        <i class="uil uil-github"></i>
+                    </a>
+                </div>
+            </div>
+
+            <p class="footer_copy">&#169; Mathéo Da Silva. Tout droit réservé</p>
+        </div>
+    </footer>
---
+++ inc/headerSubMenu.php
@@ -0,0 +1,64 @@
+<header class="header" id="header">
+    <nav class="nav container">
+        <a href="#" class="nav_logo">Mathéo Da Silva</a>
+
+        <div class="nav_menu" id="nav-menu">
+            <ul class="nav_list grid">
+                <li class="nav_item">
+                    <a href="../index.php#home" class="nav_link" data-lang="home">
+                        <!-- icon de maison-->
+                        <i class="uil uil-house-user nav_icon"></i>
+                        Accueil
+                    </a>
+                </li>
+                <li class="nav_item">
+                    <a href="#about" class="nav_link" data-lang="about">
+                        <!-- icon d'une personne-->
+                        <i class="uil uil-user nav_icon"></i>
+                        A propos
+                    </a>
+                </li>
+                <li class="nav_item">
+                    <a href="#skills" class="nav_link" data-lang="skills">
+                        <!-- icon d'une fiche-->
+                        <i class="uil uil-file-alt nav_icon"></i>
+                        Compétences
+                    </a>
+                </li>
+
+                <li class="nav_item">
+                    <a href="#experience" class="nav_link" data-lang="experience">
+                        <!-- icon d'un logo info-->
+                        <i class="uil uil-info-circle nav_icon"></i>
+                        Parcours
+                    </a>
+                </li>
+
+                <li class="nav_item">
+                    <a href="#projects" class="nav_link" data-lang="projects">
+                        <!-- icon d'une dossier-->
+                        <i class="uil uil-folder-exclamation nav_icon"></i>
+                        Projets
+                    </a>
+                </li>
+                <li class="nav_item">
+                    <a href="#contact" class="nav_link" data-lang="contact">
+                        <!-- icon d'un message-->
+                        <i class="uil uil-message nav_icon"></i>
+                        Contactez-moi
+                    </a>
+                </li>
+            </ul>
+            <i class="uil uil-multiply nav_close" id="nav-close"></i>
+        </div>
+
+        <div class="nav_btns">
+            <!-- Changer de thèmes -->
+            <i class="uil uil-moon change-theme" id="theme-btn"></i>
+
+            <div class="nav_toggle" id="nav-toggle">
+                <i class="uil uil-list-ul toggle_icon"></i>
+            </div>
+        </div>
+    </nav>
+</header>
