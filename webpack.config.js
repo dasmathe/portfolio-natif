7b0cfbfd507ecaa8290a8a23378fcdff7207fdfa
dasmathe <da.matheo@hotmail.com>
2025-02-25 17:26:57 +0100

    ADD styles & js & img

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './assets/js/main.js',
+    output: {
+        filename: 'main-bundle.js',
+        path: path.resolve(__dirname, 'assets/js'),
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false,
+            }),
+            new CssMinimizerPlugin(),
+        ],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles.min.css',
+        }),
+    ],
+};
